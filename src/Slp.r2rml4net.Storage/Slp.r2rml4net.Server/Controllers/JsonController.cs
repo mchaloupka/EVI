ba7f70e6762c59cd197b19d013855f725d38512d
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2014-02-19 16:20:08 +0100

    Query page

--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/JsonController.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/JsonController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Slp.r2rml4net.Server.Models.Json;
 using Slp.r2rml4net.Server.R2RML;
+using VDS.RDF;
+using VDS.RDF.Query;
 
 namespace Slp.r2rml4net.Server.Controllers
 {
@@ -23,8 +26,6 @@ namespace Slp.r2rml4net.Server.Controllers
                 result.Data = data.Select(x => new GraphUriModel(x, urlHelper)).ToList();
                 result.Success = true;
                 return Json(result);
-
-
             }
             catch (Exception e)
             {
@@ -32,6 +33,92 @@ namespace Slp.r2rml4net.Server.Controllers
                 result.ExceptionMessage = e.Message;
                 result.ExceptionName = e.GetType().Name;
                 result.Data = null;
+                return Json(result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GetQueryResult(string query)
+        {
+            var result = new JsonResultModel<QueryResultModel>();
+            result.Data = new QueryResultModel();
+
+            try
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                var data = StorageWrapper.Storage.Query(query);
+                sw.Stop();
+
+                result.Data.TimeElapsedInMs = sw.ElapsedMilliseconds;
+
+                if (data is Graph)
+                {
+                    var g = (Graph)data;
+
+                    foreach (var triple in g.Triples)
+                    {
+                        var row = new QueryResultRowModel();
+                        row.Bindings.Add(new QueryResultColumnModel()
+                        {
+                            Variable = "predicate",
+                            Value = triple.Predicate.ToString()
+                        });
+
+                        row.Bindings.Add(new QueryResultColumnModel()
+                        {
+                            Variable = "subject",
+                            Value = triple.Subject.ToString()
+                        });
+
+                        row.Bindings.Add(new QueryResultColumnModel()
+                        {
+                            Variable = "object",
+                            Value = triple.Object.ToString()
+                        });
+                        result.Data.Rows.Add(row);
+                    }
+
+                    result.Data.Variables.Add("subject");
+                    result.Data.Variables.Add("predicate");
+                    result.Data.Variables.Add("object");
+                }
+                else if (data is SparqlResultSet)
+                {
+                    var resData = (SparqlResultSet)data;
+
+                    foreach (var rowData in resData)
+                    {
+                        var row = new QueryResultRowModel();
+
+                        foreach (var variable in rowData.Variables)
+                        {
+                            row.Bindings.Add(new QueryResultColumnModel()
+                            {
+                                Variable = variable,
+                                Value = rowData[variable].ToString()
+                            });
+                        }
+
+                        result.Data.Rows.Add(row);
+                    }
+
+                    result.Data.Variables.AddRange(resData.Variables);
+                }
+                else
+                {
+                    throw new Exception("Unknown query result");
+                }
+
+                result.Success = true;
+                return Json(result);
+            }
+            catch (Exception e)
+            {
+                result.Success = false;
+                result.Data = null;
+                result.ExceptionMessage = e.Message;
+                result.ExceptionName = e.GetType().Name;
                 return Json(result);
             }
         }
---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Models/Json/QueryResultModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Slp.r2rml4net.Server.Models.Json
+{
+    public class QueryResultModel
+    {
+        public List<string> Variables { get; set; }
+
+        public long TimeElapsedInMs { get; set; }
+
+        public List<QueryResultRowModel> Rows { get; set; }
+
+        public QueryResultModel()
+        {
+            this.Rows = new List<QueryResultRowModel>();
+            this.Variables = new List<string>();
+        }
+    }
+
+    public class QueryResultRowModel
+    {
+        public List<QueryResultColumnModel> Bindings { get; set; }
+
+        public QueryResultRowModel()
+        {
+            this.Bindings = new List<QueryResultColumnModel>();
+        }
+    }
+
+    public class QueryResultColumnModel
+    {
+        public string Variable { get; set; }
+
+        public string Value { get; set; }
+    }
+}
