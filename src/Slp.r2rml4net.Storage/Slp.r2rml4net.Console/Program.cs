38c29f5f4fae41cfd4a484c69c4c6171f249170f
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2014-02-23 19:51:54 +0100

    Database, r2rml definition, console app for easy queries

---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Console/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Slp.r2rml4net.Console.R2RML;
+using VDS.RDF;
+using VDS.RDF.Query;
+
+namespace Slp.r2rml4net.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StorageWrapper.AppStart();
+
+            if (StorageWrapper.StartException != null)
+            {
+                PrintException(StorageWrapper.StartException);
+            }
+            else
+            {
+                try
+                {
+                    ProcessQuery();
+                }
+                catch (Exception e)
+                {
+                    PrintException(e);
+                }
+            }
+
+            StorageWrapper.AppEnd();
+        }
+
+        private static void ProcessQuery()
+        {
+            var query = string.Empty;
+
+            using (var fs = new FileStream("query.rq", FileMode.Open, FileAccess.Read))
+            using (var sr = new StreamReader(fs))
+            {
+                query = sr.ReadToEnd();
+            }
+
+            var result = StorageWrapper.Storage.Query(query);
+
+            if (result is SparqlResultSet)
+            {
+                //Print out the Results
+                SparqlResultSet rset = (SparqlResultSet)result;
+                foreach (SparqlResult res in rset)
+                {
+                    System.Console.WriteLine(res.ToString());
+                }
+            }
+            else if (result is IGraph)
+            {
+                //Print out the Results
+                IGraph g = (IGraph)result;
+                foreach (Triple t in g.Triples)
+                {
+                    System.Console.WriteLine(t.ToString());
+                }
+            }
+        }
+
+        private static void PrintException(Exception exception)
+        {
+            System.Console.WriteLine("{0}: {1}", exception.GetType().Name, exception.Message);
+        }
+    }
+}
---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Console/R2RML/StorageWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Slp.r2rml4net.Storage;
+using Slp.r2rml4net.Storage.Sql.Vendor;
+using TCode.r2rml4net;
+using TCode.r2rml4net.Mapping.Fluent;
+
+namespace Slp.r2rml4net.Console.R2RML
+{
+    public class StorageWrapper
+    {
+        private static R2RMLStorage _storage = null;
+
+        public static R2RMLStorage Storage { get { return _storage; } }
+
+        public static void AppStart()
+        {
+            StartException = null;
+
+            try
+            {
+                var mappingPath = System.Configuration.ConfigurationManager.AppSettings["r2rmlConfig"];
+                var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["r2rmlstoreconnection"].ConnectionString;
+
+                IR2RML mapping = null;
+
+                var path = mappingPath;
+
+                using (var fs = new FileStream(path, FileMode.Open))
+                {
+                    mapping = R2RMLLoader.Load(fs);
+                }
+
+                _storage = new R2RMLStorage(mapping, new MSSQLDb(connectionString));
+            }
+            catch (Exception e)
+            {
+                StartException = e;
+            }
+        }
+
+        public static void AppEnd()
+        {
+            if (_storage != null)
+                _storage.Dispose();
+        }
+
+        public static Exception StartException { get; private set; }
+    }
+}
