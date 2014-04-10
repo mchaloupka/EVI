d05bc1c8226de671832f83468b902233ee62bfa4
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2014-04-10 20:36:25 +0200

    Completing R2RMLOptimizer

--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Optimization/SparqlAlgebra/R2RMLOptimizer.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Optimization/SparqlAlgebra/R2RMLOptimizer.cs
@@ -70,22 +70,65 @@ namespace Slp.r2rml4net.Storage.Optimization.SparqlAlgebra
 
         private bool CanSubjectMatch(BgpOp bgpOp, QueryContext context)
         {
-            // TODO: Implement this
+            var pattern = bgpOp.SubjectPattern;
+
+            if(pattern is NodeMatchPattern)
+            {
+                var nmp = (NodeMatchPattern)pattern;
+                var r2rmlDef = (ITermMap)bgpOp.R2RMLSubjectMap;
+
+                return CanMatch(nmp, r2rmlDef);
+            }
+
             return true;
         }
 
         private bool CanObjectMatch(BgpOp bgpOp, QueryContext context)
         {
-            // TODO: Implement this
+            var pattern = bgpOp.ObjectPattern;
+            ITermMap r2rmlDef = null;
+
+            if(bgpOp.R2RMLObjectMap != null)
+            {
+                r2rmlDef = bgpOp.R2RMLObjectMap;
+            }
+            else if(bgpOp.R2RMLRefObjectMap != null)
+            {
+                var parentTriples = GetParentTriplesMap(context, bgpOp.R2RMLRefObjectMap);
+                r2rmlDef = parentTriples.SubjectMap;
+            }
+            else
+            {
+                throw new Exception("R2RMLObjectMap or R2RMLRefObjectMap must be assigned");
+            }
+            
+            if(pattern is NodeMatchPattern)
+            {
+                return CanMatch((NodeMatchPattern)pattern, r2rmlDef);
+            }
+
             return true;
+        }
+
+        private ITriplesMap GetParentTriplesMap(QueryContext context, IRefObjectMap refObjectPatern)
+        {
+            // TODO: Remove this method as soon as the reference will be public
+
+            var subjectMap = refObjectPatern.SubjectMap;
+
+            foreach (var tripleMap in context.Mapping.Mapping.TriplesMaps)
+            {
+                if (tripleMap.SubjectMap == subjectMap)
+                    return tripleMap;
+            }
+
+            throw new Exception("Triples map not found");
         }
 
         private bool CanMatch(NodeMatchPattern nmp, ITermMap r2rmlTerm)
         {
             var node = nmp.Node;
-            // TODO: Has node uri?
-            // http://dotnetrdf.org/API/dotNetRDF~VDS.RDF.INode.html
-
+            
             if (r2rmlTerm.IsConstantValued)
             {
                 if (r2rmlTerm is IUriValuedTermMap)
@@ -98,13 +141,36 @@ namespace Slp.r2rml4net.Storage.Optimization.SparqlAlgebra
 
                         return UriEquals(uriValued.URI, uNode.Uri);
                     }
+                    else
+                    {
+                        return false;
+                    }
                 }
                 else if (r2rmlTerm is IObjectMap)
                 {
-                    // TODO: Compare literal terms or uri
+                    var objectMap = (IObjectMap)r2rmlTerm;
+
+                    if (objectMap.URI != null)
+                    {
+                        if(node is UriNode)
+                        {
+                            var uNode = (UriNode)node;
+
+                            return UriEquals(objectMap.URI, uNode.Uri);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (objectMap.Literal != null)
+                    {
+                        // TODO: Static literal comparison
+                    }
+                    else
+                        throw new Exception("IObjectMap must have URI or Literal assigned.");
                 }
             }
-            // TODO: Template compare
 
             return true;
         }
