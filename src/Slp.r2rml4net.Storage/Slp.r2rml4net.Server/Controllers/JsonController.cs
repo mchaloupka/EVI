e8e2541f2b11a5bade1d6f5c8d98a7d883b60417
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2014-06-27 19:25:50 +0200

    Removing the graph pages

--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/JsonController.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/JsonController.cs
@@ -15,29 +15,6 @@ namespace Slp.r2rml4net.Server.Controllers
     public class JsonController : Controller
     {
         [HttpPost]
-        public ActionResult GetGraphList()
-        {
-            var result = new JsonResultModel<List<GraphUriModel>>();
-
-            try
-            {
-                var urlHelper = GetUrlHelper();
-                var data = StorageWrapper.Storage.ListGraphs().ToList();
-                result.Data = data.Select(x => new GraphUriModel(x, urlHelper)).ToList();
-                result.Success = true;
-                return Json(result);
-            }
-            catch (Exception e)
-            {
-                result.Success = false;
-                result.ExceptionMessage = e.Message;
-                result.ExceptionName = e.GetType().Name;
-                result.Data = null;
-                return Json(result);
-            }
-        }
-
-        [HttpPost]
         [ValidateInput(false)]
         public ActionResult GetQueryResult(string query)
         {
--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/MainController.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Server/Controllers/MainController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Slp.r2rml4net.Server.Models.Main;
 
 namespace Slp.r2rml4net.Server.Controllers
 {
@@ -19,16 +18,6 @@ namespace Slp.r2rml4net.Server.Controllers
         public ActionResult Query()
         {
             return View();
-        }
-
-        public ActionResult GraphList()
-        {
-            return View();
-        }
-
-        public ActionResult Graph(Uri uri)
-        {
-            return View(new GraphModel(uri));
         }
 
         public ActionResult AppStartFailed()
