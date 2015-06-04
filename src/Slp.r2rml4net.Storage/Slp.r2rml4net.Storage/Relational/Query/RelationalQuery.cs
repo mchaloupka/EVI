a5ee50e36dd23f22389eb81aaf45bf48ba90d738
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2015-06-04 17:35:12 +0200

    Creating relational calculus structure

---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Relational/Query/RelationalQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slp.r2rml4net.Storage.Relational.Query
+{
+    public class RelationalQuery
+    {
+        public RelationalQuery(ICalculusSource model, IEnumerable<IValueBinder> valueBinders)
+        {
+            this.Model = model;
+            this.ValueBinders = valueBinders;
+        }
+
+        public ICalculusSource Model { get; private set; }
+
+        public IEnumerable<IValueBinder> ValueBinders { get; private set; }
+    }
+}
---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Relational/Query/Source/CalculusModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slp.r2rml4net.Storage.Relational.Query.Source
+{
+    public class CalculusModel 
+        : ICalculusSource
+    {
+        public CalculusModel(IEnumerable<ICalculusVariable> variables, IEnumerable<ICondition> conditions)
+        {
+            this.Variables = variables;
+            this.Conditions = conditions;
+        }
+
+        public IEnumerable<ICalculusVariable> Variables { get; private set; }
+
+        public IEnumerable<ICondition> Conditions { get; private set; }
+    }
+}
