e094505d61fd8bc5d96dd3ecaf2672e00ef16448
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2015-07-18 09:52:42 +0200

    Creating base source transformer template

--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Relational/Utils/BaseSourceTransformer.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Relational/Utils/BaseSourceTransformer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Slp.r2rml4net.Storage.Relational.Query;
 using Slp.r2rml4net.Storage.Relational.Query.Source;
+using Slp.r2rml4net.Storage.Relational.Utils.CodeGeneration;
 using Slp.r2rml4net.Storage.Sparql.Algebra;
 using Slp.r2rml4net.Storage.Utils;
 
@@ -15,65 +16,41 @@ namespace Slp.r2rml4net.Storage.Relational.Utils
     /// </summary>
     /// <typeparam name="T">Type of parameter passed to process</typeparam>
     public class BaseSourceTransformer<T>
-        : ICalculusSourceVisitor
+        : BaseSourceTransformerG<T, ICalculusSource>
     {
         /// <summary>
-        /// Transforms the calculus source.
+        /// Fallback variant for the transformation.
         /// </summary>
-        /// <param name="source">The source.</param>
-        /// <param name="data">The data.</param>
-        /// <returns>The transformed calculus source.</returns>
-        public ICalculusSource TransformCalculusSource(ICalculusSource source, T data)
+        /// <param name="toTransform">Instance to be transformed.</param>
+        /// <param name="data">The passed data.</param>
+        /// <typeparam name="TI">Type of the instance</typeparam>
+        /// <returns>The transformation result</returns>
+        protected override ICalculusSource CommonFallbackTransform<TI>(TI toTransform, T data)
         {
-            return (ICalculusSource) source.Accept(this, data);
+            throw new Exception("This code should not be reached");
         }
 
         /// <summary>
-        /// Process the <see cref="CalculusModel"/>
+        /// Process the <see cref="CalculusModel" />
         /// </summary>
-        /// <param name="calculusModel">The instance to process</param>
+        /// <param name="toTransform">The instance to process</param>
         /// <param name="data">The passed data</param>
-        /// <returns></returns>
-        protected virtual ICalculusSource Process(CalculusModel calculusModel, T data)
+        /// <returns>The transformation result</returns>
+        protected override ICalculusSource Transform(CalculusModel toTransform, T data)
         {
-            return calculusModel;
+            return toTransform;
         }
 
         /// <summary>
-        /// Process the <see cref="SqlTable"/>
+        /// Process the <see cref="SqlTable" />
         /// </summary>
-        /// <param name="sqlTable">The instance to process</param>
+        /// <param name="toTransform">The instance to process</param>
         /// <param name="data">The passed data</param>
-        /// <returns></returns>
-        protected virtual ICalculusSource Process(SqlTable sqlTable, T data)
+        /// <returns>The transformation result</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        protected override ICalculusSource Transform(SqlTable toTransform, T data)
         {
-            return sqlTable;
+            return toTransform;
         }
-
-        #region ICalculusSourceVisitor
-        /// <summary>
-        /// Visits <see cref="CalculusModel" />
-        /// </summary>
-        /// <param name="calculusModel">The visited instance</param>
-        /// <param name="data">The passed data</param>
-        /// <returns>The returned data</returns>
-        public object Visit(CalculusModel calculusModel, object data)
-        {
-            // TODO: Process child sources
-            
-            return Process(calculusModel, (T)data);
-        }
-
-        /// <summary>
-        /// Visits <see cref="SqlTable" />
-        /// </summary>
-        /// <param name="sqlTable">The visited instance</param>
-        /// <param name="data">The passed data</param>
-        /// <returns>The returned data</returns>
-        public object Visit(SqlTable sqlTable, object data)
-        {
-            return Process(sqlTable, (T)data);
-        } 
-        #endregion
     }
 }
---
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Storage/Relational/Utils/CodeGeneration/BaseSourceTransformerG.cs
@@ -0,0 +1,186 @@
+﻿// This is generated code, do not edit!!!
+
+using Slp.r2rml4net.Storage.Relational.Query;
+using Slp.r2rml4net.Storage.Relational.Query.Source;
+
+namespace Slp.r2rml4net.Storage.Relational.Utils.CodeGeneration
+{
+    /// <summary>
+    /// Base generated transformer for <see cref="ICalculusSourceVisitor" />
+    /// </summary>
+    /// <typeparam name="T">Type of parameter passed to process</typeparam>
+    /// <typeparam name="TR">Type of the transformation result</typeparam>
+    public abstract class BaseSourceTransformerG<T, TR>
+        : ICalculusSourceVisitor
+    {
+        /// <summary>
+        /// Transforms the <see cref="ICalculusSource" />.
+        /// </summary>
+        /// <param name="instance">The instance to tranform.</param>
+        /// <param name="data">The passed data.</param>
+        /// <returns>The transformed calculus source.</returns>
+        public TR Transform(ICalculusSource instance, T data)
+        {
+            return (TR)instance.Accept(this, data);
+        }
+
+        /// <summary>
+        /// Preprocess for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed</param>
+        /// <param name="data">The passed data</param>
+        /// <typeparam name="TI">Type of the instance</typeparam>
+        /// <returns><c>true</c> if transformation should continue, <c>false</c> the fallback should be used.</returns>
+        protected virtual bool CommonPreTransform<TI>(ref TI toTransform, T data) where TI : ICalculusSource
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Postprocess for the transformation.
+        /// </summary>
+        /// <param name="transformed">The transformation result.</param>
+        /// <param name="toTransform">The transformed instance</param>
+        /// <param name="data">The passed data.</param>
+        /// <typeparam name="TI">Type of the instance</typeparam>
+        /// <returns>The postprocessed transformation result</returns>
+        protected virtual TR CommonPostTransform<TI>(TR transformed, TI toTransform, T data) where TI : ICalculusSource
+        {
+            return transformed;
+        }
+
+        /// <summary>
+        /// Fallback variant for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed.</param>
+        /// <param name="data">The passed data.</param>
+        /// <typeparam name="TI">Type of the instance</typeparam>
+        /// <returns>The transformation result</returns>
+        protected abstract TR CommonFallbackTransform<TI>(TI toTransform, T data) where TI : ICalculusSource;
+
+        /// <summary>
+        /// Visits <see cref="CalculusModel" />
+        /// </summary>
+        /// <param name="toVisit">The visited instance</param>
+        /// <param name="data">The passed data</param>
+        /// <returns>The returned data</returns>
+        public object Visit(CalculusModel toVisit, object data)
+        {
+            var tData = (T)data;
+            if(PreTransform(ref toVisit, tData))
+            {
+                var transformed = Transform(toVisit, tData);
+                return PostTransform(transformed, toVisit, tData);
+            }
+            else
+            {
+                return FallbackTransform(toVisit, tData);
+            }
+        }
+
+        /// <summary>
+        /// Process the <see cref="CalculusModel"/>
+        /// </summary>
+        /// <param name="toTransform">The instance to process</param>
+        /// <param name="data">The passed data</param>
+        /// <returns>The transformation result</returns>
+        protected abstract TR Transform(CalculusModel toTransform, T data);
+
+        /// <summary>
+        /// Preprocess for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed</param>
+        /// <param name="data">The passed data</param>
+        /// <returns><c>true</c> if transformation should continue, <c>false</c> the fallback should be used.</returns>
+        protected virtual bool PreTransform(ref CalculusModel toTransform, T data)
+        {
+            return CommonPreTransform(ref toTransform, data);
+        }
+
+        /// <summary>
+        /// Postprocess for the transformation.
+        /// </summary>
+        /// <param name="transformed">The transformation result.</param>
+        /// <param name="toTransform">The transformed instance</param>
+        /// <param name="data">The passed data.</param>
+        /// <returns>The postprocessed transformation result</returns>
+        protected virtual TR PostTransform(TR transformed, CalculusModel toTransform, T data)
+        {
+            return CommonPostTransform(transformed, toTransform, data);
+        }
+
+        /// <summary>
+        /// Fallback variant for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed.</param>
+        /// <param name="data">The passed data.</param>
+        /// <returns>The transformation result</returns>
+        protected virtual TR FallbackTransform(CalculusModel toTransform, T data)
+        {
+            return CommonFallbackTransform(toTransform, data);
+        }
+
+        /// <summary>
+        /// Visits <see cref="SqlTable" />
+        /// </summary>
+        /// <param name="toVisit">The visited instance</param>
+        /// <param name="data">The passed data</param>
+        /// <returns>The returned data</returns>
+        public object Visit(SqlTable toVisit, object data)
+        {
+            var tData = (T)data;
+            if(PreTransform(ref toVisit, tData))
+            {
+                var transformed = Transform(toVisit, tData);
+                return PostTransform(transformed, toVisit, tData);
+            }
+            else
+            {
+                return FallbackTransform(toVisit, tData);
+            }
+        }
+
+        /// <summary>
+        /// Process the <see cref="SqlTable"/>
+        /// </summary>
+        /// <param name="toTransform">The instance to process</param>
+        /// <param name="data">The passed data</param>
+        /// <returns>The transformation result</returns>
+        protected abstract TR Transform(SqlTable toTransform, T data);
+
+        /// <summary>
+        /// Preprocess for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed</param>
+        /// <param name="data">The passed data</param>
+        /// <returns><c>true</c> if transformation should continue, <c>false</c> the fallback should be used.</returns>
+        protected virtual bool PreTransform(ref SqlTable toTransform, T data)
+        {
+            return CommonPreTransform(ref toTransform, data);
+        }
+
+        /// <summary>
+        /// Postprocess for the transformation.
+        /// </summary>
+        /// <param name="transformed">The transformation result.</param>
+        /// <param name="toTransform">The transformed instance</param>
+        /// <param name="data">The passed data.</param>
+        /// <returns>The postprocessed transformation result</returns>
+        protected virtual TR PostTransform(TR transformed, SqlTable toTransform, T data)
+        {
+            return CommonPostTransform(transformed, toTransform, data);
+        }
+
+        /// <summary>
+        /// Fallback variant for the transformation.
+        /// </summary>
+        /// <param name="toTransform">Instance to be transformed.</param>
+        /// <param name="data">The passed data.</param>
+        /// <returns>The transformation result</returns>
+        protected virtual TR FallbackTransform(SqlTable toTransform, T data)
+        {
+            return CommonFallbackTransform(toTransform, data);
+        }
+
+    }
+}
