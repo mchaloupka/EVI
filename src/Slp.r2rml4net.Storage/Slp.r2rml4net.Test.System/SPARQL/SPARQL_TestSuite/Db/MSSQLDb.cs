dd689f0b0a031dc80ee2e5ba6a24cbd47140bcab
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2015-07-24 17:07:47 +0200

    XML definition of a dataset

--- src/Slp.r2rml4net.Storage/Slp.r2rml4net.Test.System/SPARQL/SPARQL_TestSuite/Db/MSSQLDb.cs
+++ src/Slp.r2rml4net.Storage/Slp.r2rml4net.Test.System/SPARQL/SPARQL_TestSuite/Db/MSSQLDb.cs
@@ -1,22 +1,48 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Slp.r2rml4net.Storage;
 using Slp.r2rml4net.Storage.Bootstrap;
 using Slp.r2rml4net.Storage.Sql;
+using TCode.r2rml4net.Mapping.Fluent;
 
 namespace Slp.r2rml4net.Test.System.SPARQL.SPARQL_TestSuite.Db
 {
     [TestClass]
     public class MSSQLDb : TestSuite
     {
-        protected override IR2RmlStorageFactory GetStorageFactory()
+        private static Dictionary<string, R2RmlStorage> _storages;
+
+        [ClassInitialize]
+        public static void TestSuiteInitialization(TestContext context)
+        {
+            _storages = new Dictionary<string, R2RmlStorage>();
+
+            foreach (var dataset in StorageNames)
+            {
+                var storage = InitializeDataset(dataset, GetSqlDb(), GetStorageFactory());
+                _storages.Add(dataset, storage);
+            }
+        }
+
+        private static ISqlDb GetSqlDb()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["mssql_connection"].ConnectionString;
+            return (new DefaultSqlDbFactory()).CreateSqlDb(connectionString);
+        }
+
+        private static IR2RmlStorageFactory GetStorageFactory()
         {
             return new DefaultIr2RmlStorageFactory();
         }
 
-        protected override ISqlDb GetSqlDb()
+        protected override R2RmlStorage GetStorage(string storageName)
         {
-            var connectionString = ConfigurationManager.ConnectionStrings["mssql_connection"].ConnectionString;
-            return (new DefaultSqlDbFactory()).CreateSqlDb(connectionString);
+            return _storages[storageName];
         }
     }
 }
