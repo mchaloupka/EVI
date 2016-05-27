0e16102e34ecb5d10813557f3be792dffe142936
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2016-05-27 12:03:12 +0200

    Moving ExpressionVisitParameter class out of the RelationalBuilder

---
+++ src/Slp.Evi.Storage/Slp.Evi.Storage/Relational/Builder/ExpressionVisitParameter.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Slp.Evi.Storage.Query;
+using Slp.Evi.Storage.Relational.Query;
+using Slp.Evi.Storage.Sparql.Algebra.Expressions;
+
+namespace Slp.Evi.Storage.Relational.Builder
+{
+    /// <summary>
+    /// Parameter passed to <see cref="ISparqlExpressionVisitor"/> visit methods.
+    /// </summary>
+    public class ExpressionVisitParameter
+    {
+        /// <summary>
+        /// Gets the query context.
+        /// </summary>
+        public QueryContext QueryContext { get; private set; }
+
+        /// <summary>
+        /// Gets the available value binders.
+        /// </summary>
+        public Dictionary<string, IValueBinder> ValueBinders { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionVisitParameter"/> class.
+        /// </summary>
+        /// <param name="queryContext">The query context.</param>
+        /// <param name="valueBinders">The value binders.</param>
+        public ExpressionVisitParameter(QueryContext queryContext, IEnumerable<IValueBinder> valueBinders)
+        {
+            QueryContext = queryContext;
+            ValueBinders = valueBinders.ToDictionary(x => x.VariableName);
+        }
+    }
+}
--- src/Slp.Evi.Storage/Slp.Evi.Storage/Relational/Builder/RelationalBuilder.cs
+++ src/Slp.Evi.Storage/Slp.Evi.Storage/Relational/Builder/RelationalBuilder.cs
@@ -648,32 +648,5 @@ namespace Slp.Evi.Storage.Relational.Builder
             var parameter = (ExpressionVisitParameter)data;
             return _expressionBuilder.CreateExpression(parameter.QueryContext, nodeExpression.Node);
         }
-
-        /// <summary>
-        /// Parameter passed to <see cref="ISparqlExpressionVisitor"/> visit methods.
-        /// </summary>
-        public class ExpressionVisitParameter
-        {
-            /// <summary>
-            /// Gets the query context.
-            /// </summary>
-            public QueryContext QueryContext { get; private set; }
-
-            /// <summary>
-            /// Gets the available value binders.
-            /// </summary>
-            public Dictionary<string, IValueBinder> ValueBinders { get; private set; }
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="ExpressionVisitParameter"/> class.
-            /// </summary>
-            /// <param name="queryContext">The query context.</param>
-            /// <param name="valueBinders">The value binders.</param>
-            public ExpressionVisitParameter(QueryContext queryContext, IEnumerable<IValueBinder> valueBinders)
-            {
-                QueryContext = queryContext;
-                ValueBinders = valueBinders.ToDictionary(x => x.VariableName);
-            }
-        }
     }
 }
