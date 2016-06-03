43ede377989db3d8d803c083426e98e49c5c7175
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2016-06-03 12:15:31 +0200

    Bind test

--- src/Slp.Evi.Storage/Slp.Evi.Test.System/SPARQL/SPARQL_TestSuite/TestSuite.cs
+++ src/Slp.Evi.Storage/Slp.Evi.Test.System/SPARQL/SPARQL_TestSuite/TestSuite.cs
@@ -80,6 +80,18 @@ namespace Slp.Evi.Test.System.SPARQL.SPARQL_TestSuite
             AssertBagEqual(expected, result);
         }
 
+        [TestMethod]
+        public void Simple_bind()
+        {
+            var storage = GetStorage("simple.xml");
+            var queryFile = @"Data\Simple\bind.rq";
+            var resultFile = @"Data\Simple\bind.srx";
+            var query = GetQuery(queryFile);
+            var result = storage.Query(query);
+            var expected = GetExpected(resultFile);
+            AssertBagEqual(expected, result);
+        }
+
 
         [TestMethod]
         public void Simple_Filter_bound()
