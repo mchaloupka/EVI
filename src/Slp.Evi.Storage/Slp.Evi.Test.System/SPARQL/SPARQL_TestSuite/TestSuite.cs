5a07e76f2205c1d1db5028fe0d3a6829883b1de3
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2016-06-19 13:30:28 +0200

    Test for nested filter

--- src/Slp.Evi.Storage/Slp.Evi.Test.System/SPARQL/SPARQL_TestSuite/TestSuite.cs
+++ src/Slp.Evi.Storage/Slp.Evi.Test.System/SPARQL/SPARQL_TestSuite/TestSuite.cs
@@ -104,6 +104,18 @@ namespace Slp.Evi.Test.System.SPARQL.SPARQL_TestSuite
             AssertBagEqual(expected, result);
         }
 
+        [TestMethod]
+        public void Simple_nested_filter()
+        {
+            var storage = GetStorage("simple.xml");
+            var queryFile = @"Data\Simple\nested_filter.rq";
+            var resultFile = @"Data\Simple\nested_filter.srx";
+            var query = GetQuery(queryFile);
+            var result = storage.Query(query);
+            var expected = GetExpected(resultFile);
+            AssertBagEqual(expected, result);
+        }
+
 
         [TestMethod]
         public void Simple_Filter_bound()
