eb21b1c882f83818463cd06751d6937d56bbe3c8
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2016-10-30 17:52:04 +0100

    Implemented type cache

--- src/Slp.Evi.Storage/Slp.Evi.Storage/Sparql/Types/TypeCache.cs
+++ src/Slp.Evi.Storage/Slp.Evi.Storage/Sparql/Types/TypeCache.cs
@@ -44,6 +44,20 @@ namespace Slp.Evi.Storage.Sparql.Types
         private readonly Dictionary<IValueType, int> _typeIndexesDictionary;
 
         /// <summary>
+        /// The created full types indexes
+        /// </summary>
+        private readonly Dictionary<string, Dictionary<string, int>> _createdFullTypesIndexes;
+
+        /// <summary>
+        /// The created language type only indexes
+        /// </summary>
+        private readonly Dictionary<string, int> _createdLangOnlyIndexes;
+        /// <summary>
+        /// The created datatype type only indexes
+        /// </summary>
+        private readonly Dictionary<string, int> _createdTypeOnlyIndexes;
+
+        /// <summary>
         /// The next index
         /// </summary>
         private int _index;
@@ -58,9 +72,13 @@ namespace Slp.Evi.Storage.Sparql.Types
             _typesDictionary = new CacheDictionary<IMapBase, IValueType>(ResolveType);
             _typesIndexDictionary = new Dictionary<int, IValueType>();
             _typeIndexesDictionary = new Dictionary<IValueType, int>();
+            _createdFullTypesIndexes = new Dictionary<string, Dictionary<string, int>>();
+            _createdLangOnlyIndexes = new Dictionary<string, int>();
+            _createdTypeOnlyIndexes = new Dictionary<string, int>();
 
             var iriType = new IRIValueType();
             var blankNodeType = new BlankValueType();
+            var noLangNoTypeLiteral = new LiteralValueType(null, null);
 
             _typesIndexDictionary.Add(1, iriType);
             _typeIndexesDictionary.Add(iriType, 1);
@@ -68,7 +86,10 @@ namespace Slp.Evi.Storage.Sparql.Types
             _typesIndexDictionary.Add(2, blankNodeType);
             _typeIndexesDictionary.Add(blankNodeType, 2);
 
-            _index = 3;
+            _typesIndexDictionary.Add(3, noLangNoTypeLiteral);
+            _typeIndexesDictionary.Add(noLangNoTypeLiteral, 3);
+
+            _index = 4;
         }
 
         /// <summary>
@@ -188,8 +209,57 @@ namespace Slp.Evi.Storage.Sparql.Types
                 }
             }
 
-            // TODO: Continue here, find existing type or create a new one
-            throw new NotImplementedException();
+            if (dataType == null && languageTag == null)
+            {
+                return _typesIndexDictionary[3];
+            }
+            else if (dataType == null)
+            {
+                int foundIndex;
+                if (!_createdLangOnlyIndexes.TryGetValue(languageTag, out foundIndex))
+                {
+                    var type = new LiteralValueType(null, languageTag);
+                    _typesIndexDictionary.Add(_index, type);
+                    _typeIndexesDictionary.Add(type, _index);
+                    _createdLangOnlyIndexes.Add(languageTag, _index);
+                    _index++;
+                    return type;
+                }
+                else
+                {
+                    return _typesIndexDictionary[foundIndex];
+                }
+            }
+            else
+            {
+                var dataTypeFullUri = dataType.ToCompleteUri();
+                var dictionary = _createdTypeOnlyIndexes;
+
+                if (languageTag != null)
+                {
+                    if (!_createdFullTypesIndexes.ContainsKey(languageTag))
+                    {
+                        _createdFullTypesIndexes.Add(languageTag, new Dictionary<string, int>());
+                    }
+
+                    dictionary = _createdFullTypesIndexes[languageTag];
+                }
+
+                int foundIndex;
+                if (!dictionary.TryGetValue(dataTypeFullUri, out foundIndex))
+                {
+                    var type = new LiteralValueType(dataTypeFullUri, languageTag);
+                    _typesIndexDictionary.Add(_index, type);
+                    _typeIndexesDictionary.Add(type, _index);
+                    dictionary.Add(dataTypeFullUri, _index);
+                    _index++;
+                    return type;
+                }
+                else
+                {
+                    return _typesIndexDictionary[foundIndex];
+                }
+            }
         }
     }
 }
