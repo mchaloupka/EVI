60484671fa39bb714fc0a7091bcd23c82b3d6ba5
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2016-11-13 20:57:04 +0100

    UnionJoinOptimizerTest fixes.

--- src/Slp.Evi.Storage/Slp.Evi.Test.Unit/Sparql/Optimization/Optimizers/UnionJoinOptimizerTest.cs
+++ src/Slp.Evi.Storage/Slp.Evi.Test.Unit/Sparql/Optimization/Optimizers/UnionJoinOptimizerTest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Slp.Evi.Storage.Query;
 using Slp.Evi.Storage.Sparql.Algebra;
 using Slp.Evi.Storage.Sparql.Algebra.Patterns;
 using Slp.Evi.Storage.Sparql.PostProcess.Optimizers;
+using Slp.Evi.Storage.Sparql.Types;
 using TCode.r2rml4net.Mapping;
 using VDS.RDF.Query.Patterns;
 
@@ -27,20 +29,31 @@ namespace Slp.Evi.Test.Unit.Sparql.Optimization.Optimizers
             var p1 = CreateTemplatedRestrictedTriplePattern("v1", "p1", "v3", "http://test.com/{id}", "http://test.com/{id}", "http://test.com/{id}");
             var p2 = CreateTemplatedRestrictedTriplePattern("v2", "p2", "v3", "http://test.com/{id}", "http://test.com/{id}", "http://test.com/{id}");
             var p3 = CreateTemplatedRestrictedTriplePattern("v3", "p3", "v4", "http://test.com/{id}", "http://test.com/{id}", "http://test.com/{id}");
-            
-            var up1 = new UnionPattern(new IGraphPattern[] {p1, p2});
 
-            var join = new JoinPattern(new IGraphPattern[] {up1, p3});
+            var up1 = new UnionPattern(new IGraphPattern[] { p1, p2 });
 
-            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext());
+            var join = new JoinPattern(new IGraphPattern[] { up1, p3 });
+
+            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext()
+            {
+                Context = GetQueryContext().Object
+            });
 
             var expected = new UnionPattern(new IGraphPattern[]
             {
                 new JoinPattern(new IGraphPattern[] {p1, p3}),
-                new JoinPattern(new IGraphPattern[] {p2, p3}),  
+                new JoinPattern(new IGraphPattern[] {p2, p3}),
             });
 
             AssertPatternsEqual(expected, result);
+        }
+
+        private static Mock<IQueryContext> GetQueryContext()
+        {
+            var queryContext = new Mock<IQueryContext>();
+            var typeCache = new Mock<ITypeCache>();
+            queryContext.SetupGet(x => x.TypeCache).Returns(typeCache.Object);
+            return queryContext;
         }
 
         [TestMethod]
@@ -54,7 +67,10 @@ namespace Slp.Evi.Test.Unit.Sparql.Optimization.Optimizers
 
             var join = new JoinPattern(new IGraphPattern[] { up1, p3 });
 
-            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext());
+            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext()
+            {
+                Context = GetQueryContext().Object
+            });
 
             var expected = new JoinPattern(new IGraphPattern[] {p1, p3});
 
@@ -74,7 +90,10 @@ namespace Slp.Evi.Test.Unit.Sparql.Optimization.Optimizers
 
             var join = new JoinPattern(new IGraphPattern[] { up1, up2 });
 
-            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext());
+            var result = _optimizerImplementation.TransformGraphPattern(join, new BaseSparqlOptimizer<object>.OptimizationContext()
+            {
+                Context = GetQueryContext().Object
+            });
 
             var expected = new UnionPattern(new IGraphPattern[]
             {
