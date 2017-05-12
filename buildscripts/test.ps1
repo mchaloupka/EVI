ed64d664ab00873903623869b2f1a0ee64a9d89e
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2017-05-12 14:37:16 +0200

    Try opencover.

--- buildscripts/test.ps1
+++ buildscripts/test.ps1
@@ -1,1 +1,2 @@
-vstest.console src\Slp.Evi.Storage\Slp.Evi.Test.Unit\bin\Release\Slp.Evi.Test.Unit.dll src\Slp.Evi.Storage\Slp.Evi.Test.System\bin\Release\Slp.Evi.Test.System.dll /logger:Appveyor /Enablecodecoverage
+choco install opencover -y
+OpenCover.Console.exe -register:user -target:"vstest.console" -targetArgs:"src\Slp.Evi.Storage\Slp.Evi.Test.Unit\bin\Release\Slp.Evi.Test.Unit.dll src\Slp.Evi.Storage\Slp.Evi.Test.System\bin\Release\Slp.Evi.Test.System.dll /logger:Appveyor" -filter:"+[Slp.Evi.Storage*]*"
