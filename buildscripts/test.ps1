6f69eaac71a0d306a5485a2633bd7ea5dff53e59
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2017-05-12 14:17:14 +0200

    Enable code coverage for tests

---
+++ buildscripts/test.ps1
@@ -0,0 +1,1 @@
+vstest.console src\Slp.Evi.Storage\Slp.Evi.Test.Unit\bin\Release\Slp.Evi.Test.Unit.dll src\Slp.Evi.Storage\Slp.Evi.Test.System\bin\Release\Slp.Evi.Test.System.dll /logger:Appveyor /Enablecodecoverage
