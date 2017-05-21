5a64caa1646f12ccc1ef43dc17b510a28aed21c8
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2017-05-21 20:35:56 +0200

    Update test.ps1

--- buildscripts/test.ps1
+++ buildscripts/test.ps1
@@ -1,2 +1,3 @@
 choco install opencover -y
 OpenCover.Console.exe -register:user -returntargetcode -target:"vstest.console.exe" -targetargs:"src\Slp.Evi.Storage\Slp.Evi.Test.Unit\bin\Release\Slp.Evi.Test.Unit.dll src\Slp.Evi.Storage\Slp.Evi.Test.System\bin\Release\Slp.Evi.Test.System.dll /logger:Appveyor" -filter:"+[Slp.Evi.Storage*]*" -output:".\coverage.xml"
+Add-AppveyorMessage -Message "Tests exit code: $LastExitCode"
