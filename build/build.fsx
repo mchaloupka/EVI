b502964860074d6cb2c13a93678befd468341c29
Milos Chaloupka <milos.chaloupka@ncr.com>
2018-05-07 17:41:11 +0200

    Some simple build script to start with

---
+++ build/build.fsx
@@ -0,0 +1,28 @@
+#r "paket:
+nuget Fake.Core.Target //"
+// include Fake modules, see Fake modules section
+
+open Fake.Core
+
+// *** Define Targets ***
+Target.create "Clean" (fun _ ->
+  Trace.log " --- Cleaning stuff --- "
+)
+
+Target.create "Build" (fun _ ->
+  Trace.log " --- Building the app --- "
+)
+
+Target.create "Deploy" (fun _ ->
+  Trace.log " --- Deploying app --- "
+)
+
+open Fake.Core.TargetOperators
+
+// *** Define Dependencies ***
+"Clean"
+  ==> "Build"
+  ==> "Deploy"
+
+// *** Start Build ***
+Target.runOrDefault "Deploy"
