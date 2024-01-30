5b1160fdaadf0bfbda0965925ea1fcb23f9030bf
Milos Chaloupka <chaloupkamilos@hotmail.com>
2024-01-30 00:18:19 +0000

    Adding MySQL Support (#35)

---
+++ src/Slp.Evi.Storage/Slp.Evi.Test.System/Sparql/SparqlTestData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Slp.Evi.Test.System.Sparql;
+
+public static class SparqlTestData
+{
+    public static string GetPath(string dataFile)
+    {
+        var root = AppDomain.CurrentDomain.BaseDirectory;
+        var path = Path.Combine(root, $".\\Sparql\\{dataFile}");
+        return path;
+    }
+
+    public static string GetQuery(string queryFile)
+    {
+        using var sr = new StreamReader(new FileStream(GetPath(queryFile), FileMode.Open, FileAccess.Read));
+        return sr.ReadToEnd();
+    }
+
+    public static IEnumerable<object[]> TestData => new List<object[]>
+    {
+        new object[] {"simple", "single", QueryVerificationType.BagEqual},
+        new object[] {"simple", "join", QueryVerificationType.BagEqual},
+        new object[] {"simple", "union", QueryVerificationType.BagEqual},
+        new object[] {"simple", "empty", QueryVerificationType.BagEqual},
+        new object[] {"simple", "null", QueryVerificationType.BagEqual},
+        new object[] {"simple", "optional", QueryVerificationType.BagEqual},
+        new object[] {"simple", "bind", QueryVerificationType.BagEqual},
+        new object[] {"simple", "nested_optional", QueryVerificationType.BagEqual},
+        new object[] {"simple", "nested_filter", QueryVerificationType.BagEqual},
+        new object[] {"simple", "distinct", QueryVerificationType.BagEqual},
+
+        new object[] {"simple", @"filter\bound", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\not_bound", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_gt", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_ge", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_lt", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_le", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_eq", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\comparison_neq", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\disjunction", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"filter\conjunction", QueryVerificationType.BagEqual},
+
+        new object[] {"simple", @"type\int", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\double", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\type_equal", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\type_comp_eq", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\type_comp_eq2", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\type_comp_gt", QueryVerificationType.BagEqual},
+        new object[] {"simple", @"type\type_join_different", QueryVerificationType.BagEqual},
+
+        new object[] {"students", "no_result", QueryVerificationType.BagEqual},
+        new object[] {"students", "student_names", QueryVerificationType.BagEqual},
+        new object[] {"students", "student_names_order", QueryVerificationType.Equal},
+        new object[] {"students", "student_names_order_desc", QueryVerificationType.Equal},
+        new object[] {"students", "student_names_order_limit", QueryVerificationType.Equal},
+        new object[] {"students", "student_names_order_offset", QueryVerificationType.Equal},
+        new object[] {"students", "student_names_order_offset_limit", QueryVerificationType.Equal},
+
+        new object[] {"bsbm", "ProductType_OrderBy", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_01", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_02", QueryVerificationType.BagEqual},
+        new object[] {"bsbm", "Query_03", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_04", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_05", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_06", QueryVerificationType.BagEqual},
+        new object[] {"bsbm", "Query_07", QueryVerificationType.BagEqual},
+        new object[] {"bsbm", "Query_08", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_09", QueryVerificationType.None},
+        new object[] {"bsbm", "Query_10", QueryVerificationType.Equal},
+        new object[] {"bsbm", "Query_11", QueryVerificationType.BagEqual},
+        new object[] {"bsbm", "Query_12", QueryVerificationType.None}
+    };
+}
---
+++ src/Slp.Evi.Storage/Slp.Evi.Test.System/Sparql/Vendor/MySqlSparqlTestSuite.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Slp.Evi.Storage.MySql;
+using Slp.Evi.Storage.MySql.Database;
+using TCode.r2rml4net;
+using Xunit;
+
+namespace Slp.Evi.Test.System.Sparql.Vendor
+{
+    public class MySqlSparqlTestSuite(MySqlSparqlFixture fixture)
+        : SparqlTestSuite<MySqlEviStorage, MySqlDatabase>(fixture), IClassFixture<MySqlSparqlFixture>;
+
+    public sealed class MySqlSparqlFixture()
+        : SparqlFixture<MySqlEviStorage, MySqlDatabase>(new MySqlSparqlTestHelpers())
+    {
+        protected override MySqlDatabase GetSqlDb() => new(GetConnectionString("MySql"), 30);
+    }
+
+    public class MySqlSparqlTestHelpers
+        : SparqlTestHelpers<MySqlEviStorage, MySqlDatabase>
+    {
+        /// <inheritdoc />
+        protected override void CreateTable(MySqlDatabase sqlDb, XElement table)
+        {
+            var tableName = table.Attribute("name").Value;
+            RunQuery(sqlDb, $"DROP TABLE IF EXISTS {tableName}");
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("CREATE TABLE ");
+            sb.Append(tableName);
+            sb.Append(" (");
+
+            bool first = true;
+
+            foreach (var tablePart in table.Elements())
+            {
+                if (!first)
+                {
+                    sb.Append(", ");
+                }
+                else
+                {
+                    first = false;
+                }
+
+                if (tablePart.Name == "column")
+                {
+                    sb.Append(tablePart.Attribute("name").Value);
+                    sb.Append(' ');
+                    sb.Append(tablePart.Attribute("mysqltype").Value);
+                    sb.Append(' ');
+
+                    if (tablePart.Attribute("nullable") != null && tablePart.Attribute("nullable").Value == "true")
+                    {
+                        sb.Append("NULL");
+                    }
+                    else
+                    {
+                        sb.Append("NOT NULL");
+                    }
+                }
+                else
+                    throw new Exception(String.Format("Unknown table part {1} when creating table {0}", tableName, tablePart.Name));
+            }
+
+            var primaryKeys = table.Elements()
+                .Where(x => x.Name == "column" && x.Attribute("primary-key")?.Value == "true")
+                .Select(x => x.Attribute("name").Value);
+
+            if (primaryKeys.Any())
+            {
+                sb.Append(", PRIMARY KEY(");
+                sb.Append(String.Join(", ", primaryKeys));
+                sb.Append(")");
+            }
+
+            sb.Append(")");
+
+            RunQuery(sqlDb, sb.ToString());
+        }
+
+        private void RunQuery(MySqlDatabase sqlDb, String query)
+        {
+            sqlDb.ExecuteQuery(new MySqlQuery(query)).Dispose();
+        }
+
+        /// <inheritdoc />
+        protected override void ExecuteQuery(MySqlDatabase sqlDb, XElement query)
+        {
+            RunQuery(sqlDb, query.Value);
+        }
+
+        /// <inheritdoc />
+        protected override MySqlEviStorage CreateStorage(IR2RML mapping, MySqlDatabase database) =>
+            new(mapping, database);
+    }
+}
